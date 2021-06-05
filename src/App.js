2a526eb23f58a7f761788a3a0c9801bdaafe8d6e
Hikmet-dev <hikmetmeh@gmail.com>
2021-06-05 21:56:43 +0300

    Create Ligin component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import axios from 'axios';
 import { LogIn } from './components/LogIn';
 
 function App() {
-  const [error, setError] = useState(null)
-  const [open, setOpen] = useState(false)
+  const [error, setError] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [auth] = useState(false);
   axios.interceptors.response.use(undefined, (error) =>  {
     if( [422, 404, 400].includes(error.response?.status) ) {
       setError(error)
@@ -17,18 +18,13 @@ function App() {
     return Promise.reject(error);
   });
     const handleClose = (event, reason) => {
-      if (reason === 'clickaway') {
-        return;
-      }
+      if (reason === 'clickaway') return;
       setOpen(false);
     };
   return (
     <Container>
-      <ToDoList />
-
-      <LogIn />
-
-
+      {auth && <ToDoList />}
+      {!auth && <LogIn />}
       {error && (<Snackbar open={open} autoHideDuration={6000} onClose={handleClose} >
                               <Alert severity="error" onClose={handleClose} > 
                               <AlertTitle>{error.name}</AlertTitle>
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,28 +1,63 @@
 import React from 'react';
-import { Formik } from 'formik';
-import { Input, Grid, Button  } from '@material-ui/core';
+import { useFormik  } from 'formik';
+import { Input, Grid, Button, FormHelperText  } from '@material-ui/core';
+
+const validate = values => {
+    const errors = {};
+    if (!values.email) errors.email = 'Required';
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) errors.email = 'Invalid email address';
+    if(!values.password) errors.password = 'Required';
+    if (values.password.length < 7) errors.password = 'Must be 8 characters or more';
+    return errors;
+};
 
 
 export const LogIn = () => {
-
-
+        const formik = useFormik({
+          initialValues: {
+            email: '',
+            password: ''
+          },
+          validate,
+          onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+          },
+        });
     return(
-        <Grid container spacing={6} justify="center">
-                    <Grid item xs={6} >
-        <Formik 
-            initialValues={{email: '', password: ''}}
-            
-        >
-
-
-
-
-        </Formik>
-
-
-
-
-        </Grid>
-        </Grid>
+    <Grid container spacing={6} justify="center">
+        <Grid item xs={6} >     
+        <form onSubmit={formik.handleSubmit}>
+            <Input 
+                placeholder="Email" 
+                id="email" 
+                name="email"
+                fullWidth
+                error={(formik.touched.email && formik.errors.email)}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur} 
+                value={formik.values.email} 
+                />
+                {(formik.touched.email && formik.errors.email) 
+                    && <FormHelperText id="component-error-text">{formik.errors.email}</FormHelperText>}
+            <Input 
+                placeholder="Password" 
+                id="password" 
+                name="password"
+                fullWidth
+                error={(formik.touched.password && formik.errors.password)} 
+                onChange={formik.handleChange} 
+                onBlur={formik.handleBlur} 
+                value={formik.values.password} 
+            />
+                {(formik.touched.password && formik.errors.password) 
+                    && <FormHelperText id="component-error-text">{formik.errors.password}</FormHelperText>}
+            <Button 
+                variant="contained" 
+                color="secondary" 
+                type="submit"
+                >Log in</Button>
+        </form>
+      </Grid>
+    </Grid>
     )
 };
